272c0e9f79641793ba9ba72cf033736d6e39f2a3
Tasnuvaleeya <tasnuva2606@gmail.com>
2018-03-18 14:46:26 +0600

    updated

---
+++ python_function/map_lambda.py
@@ -0,0 +1,7 @@
+cube = lambda x: x**3 # complete the lambda function
+
+fib = lambda y: y if y < 2 else fib(y - 1) + fib(y - 2)
+# print(list(map(lambda x: x**3, map(fib, range(int(input()))))))
+
+out = list(map(fib, range(int(input()))))
+print(list(map(cube, out)))
